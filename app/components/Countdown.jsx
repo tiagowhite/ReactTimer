8387183d386183f2a1063da20352e6c3a970faa9
Tiago Fernandes <tiagowhite@gmail.com>
2017-02-28 22:05:26 -0300

    fix status error

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -19,7 +19,7 @@ var Countdown = React.createClass({
 					this.startTimer();
 					break;
                 case 'stopped':
-                    this.setState({count: 0});                    
+                    this.setState({count: 0});
                 case 'paused':
                     clearInterval(this.timer);
                     this.timer = undefined;
@@ -28,12 +28,21 @@ var Countdown = React.createClass({
 		}
 	},
 
-	startTimer: function () {
+    componentWillUnmount: function () {
+
+        clearInterval(this.timer);
+        this.timer = undefined;
+    },
+
+    startTimer: function () {
 		this.timer = setInterval(() => {
 			var newCount = this.state.count -1;
 			this.setState({
 				count: newCount >= 0 ? newCount : 0
 			});
+			if (newCount === 0) {
+				this.setState({countdownStatus: 'stopped'})
+			}
 		}, 1000);
 	},
     handleSetCountdown: function (seconds) {
